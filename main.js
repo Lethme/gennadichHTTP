71e9ebcd5f27fbc5d3636c01ba98f224637f1a5e
Ashe <yurirussu.mail@gmail.com>
2021-02-19 22:21:33 +0300

    test

--- main.js
+++ main.js
@@ -3,13 +3,34 @@ const url = require('url');
 const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 
 let mainWindow;
+let testWindow;
 
 const mainMenuTemplate = [{
     label: 'File',
     submenu: [{
         label: 'Test',
         click() {
+            testWindow = new BrowserWindow({
+                width: 400,
+                height: 250,
+                icon: __dirname + "/assets/img/icon.png",
+                slashes: true,
+                webPreferences: {
+                    nativeWindowOpen: true,
+                    nodeIntegrationInWorker: true,
+                    nodeIntegration: true
+                }
+            });
 
+            testWindow.loadURL(url.format({
+                pathname: path.join(__dirname, 'form.html'),
+                protocol: 'file:',
+                slashes: true,
+            }));
+
+            testWindow.on('close', () => {
+                testWindow = null;
+            });
         }
     }, {
         label: 'Exit',
@@ -60,7 +81,12 @@ app.on('ready', () => {
 
     mainWindow.on('closed', () => {
         win = null;
+        app.quit();
     });
+});
+
+ipcMain.on('item:test', (e, item) => {
+    mainWindow.webContents.send('item:test', item);
 });
 
 app.on('window-all-closed', () => {
